0dd7d568dce3e9f6ce9aa55a2d5422b78a048334
Unknown <patrick.vogt@fh-bielefeld.de>
2018-08-04 15:12:46 +0200

    add base project
    simple recording of mp3 implemented

---
+++ SpotifyRecorder/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using SpotifyRecorder.src;
+using System.Windows;
+
+namespace SpotifyRecorder
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AudioRecorder recorder = new AudioRecorder();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            btnStart.IsEnabled = false;
+            recorder.StartRecording();
+            btnStop.IsEnabled = true;
+
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            btnStop.IsEnabled = false;
+            recorder.StopRecording();
+
+
+            btnStart.IsEnabled = true;
+
+        }
+    }
+}
---
+++ SpotifyRecorder/src/AudioRecorder.cs
@@ -0,0 +1,87 @@
+﻿using NAudio.Lame;
+using NAudio.Wave;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace SpotifyRecorder.src
+{
+    class AudioRecorder
+    {
+        private WasapiLoopbackCapture capture;
+        private WaveFileWriter writer;
+        string outputFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "NAudio");
+        string outputFileName = "recorded";
+        string outputFilePathWav => Path.Combine(outputFolder, $"{outputFileName}.wav");
+        string outputFilePathMP3 => Path.Combine(outputFolder, $"{outputFileName}.mp3");
+
+        public void StartRecording()
+        {
+            Directory.CreateDirectory(outputFolder);
+
+            capture = new WasapiLoopbackCapture();
+            writer = new WaveFileWriter(outputFilePathWav, capture.WaveFormat);
+            capture.DataAvailable += (s, a) =>
+            {
+                writer.Write(a.Buffer, 0, a.BytesRecorded);
+                if (writer.Position > capture.WaveFormat.AverageBytesPerSecond * 20)
+                {
+                    capture.StopRecording();
+                }
+            };
+            capture.RecordingStopped += (s, a) =>
+            {
+                writer.Dispose();
+                writer = null;
+                capture.Dispose();
+            };
+            capture.StartRecording();
+            new Thread(() =>
+            {
+                while (capture.CaptureState != NAudio.CoreAudioApi.CaptureState.Stopped)
+                {
+                    Thread.Sleep(500);
+                }
+            }).Start();
+        }
+
+        public void StopRecording()
+        {
+            capture?.StopRecording();
+            new Thread(() =>
+            {
+                // TODO check if file is in use
+                while (writer != null)
+                {
+                    Thread.Sleep(50);
+                }
+                WaveToMP3(outputFilePathWav, outputFilePathMP3);
+            }).Start();
+        }
+
+        // Convert WAV to MP3 using libmp3lame library
+        public static void WaveToMP3(string waveFileName, string mp3FileName, int bitRate = 320)
+        {
+            using (var reader = new AudioFileReader(waveFileName))
+            {
+                using (var writer = new LameMP3FileWriter(mp3FileName, reader.WaveFormat, bitRate))
+                {
+                    reader.CopyTo(writer);
+                }
+            }
+
+        }
+
+        // Convert MP3 file to WAV using NAudio classes only
+        public static void MP3ToWave(string mp3FileName, string waveFileName)
+        {
+            using (var reader = new Mp3FileReader(mp3FileName))
+            {
+                using (var writer = new WaveFileWriter(waveFileName, reader.WaveFormat))
+                {
+                    reader.CopyTo(writer);
+                }
+            }
+        }
+    }
+}
